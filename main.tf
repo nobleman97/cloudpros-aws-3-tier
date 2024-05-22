6583e0d31e8ef2d6dfdce39431bf5320f3b1512d
David Omokhodion <david.omokhodion@perizer.com>
2024-05-22 11:58:28 +0100

    simplify module

--- main.tf
+++ main.tf
@@ -7,222 +7,26 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "public" {
+  count = length(var.public_subnets)
+
   vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_subnet_cidr
-  availability_zone = "us-east-1a"
+  cidr_block = var.public_subnets[count.index].cidr_block
+  availability_zone = var.public_subnets[count.index].availability_zone
 
   tags = {
-    Name = "${var.name_prefix}-public-subnet"
+    Name = "${var.name_prefix}-${var.public_subnets[count.index]}-public-subnet"
   }
 }
 
 resource "aws_subnet" "private" {
+  count = length(var.private_subnets)
+
   vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private_subnet_cidr
-  availability_zone = "us-east-1a"
+  cidr_block        = var.public_subnets[count.index].cidr_block
+  availability_zone = var.public_subnets[count.index].availability_zone
 
   tags = {
-    Name = "${var.name_prefix}-private-subnet"
+    Name = "${var.name_prefix}-${var.private_subnets[count.index]}-private-subnet"
   }
 }
 
-resource "aws_subnet" "private_2" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.private_2_subnet_cidr
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "${var.name_prefix}-private-subnet-2"
-  }
-}
-
-resource "aws_eip" "nat_gw_pip" {
-  tags = {
-    Name = "${var.name_prefix}-nat_gw_pip"
-  }
-}
-
-resource "aws_nat_gateway" "main" {
-  allocation_id = aws_eip.nat_gw_pip.id
-  subnet_id     = aws_subnet.public.id
-
-  tags = {
-    Name = "${var.name_prefix}-nat_gw"
-  }
-}
-
-resource "aws_route_table" "main" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.main.id
-  }
-
-  tags = {
-    Name = "${var.name_prefix}-main-rt"
-  }
-}
-
-resource "aws_main_route_table_association" "main" {
-  vpc_id         = aws_vpc.main.id
-  route_table_id = aws_route_table.main.id
-}
-
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "${var.name_prefix}-internet-gw"
-  }
-}
-
-resource "aws_route_table" "public_subnet" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-
-  tags = {
-    Name = "${var.name_prefix}-public-subnet-rt"
-  }
-}
-
-resource "aws_route_table_association" "public_subnet" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public_subnet.id
-}
-
-resource "aws_security_group" "allow_public_instance_tls" {
-  name        = "allow_public_instance_tls"
-  description = "Allow TLS inbound traffic for Public Instances"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "MSSQL from Internet"
-    from_port   = 1433
-    to_port     = 1433
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description      = "HTTP from Internet"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    description = "Oracle-RDS from Internet"
-    from_port   = 1521
-    to_port     = 1521
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "SSH from Internet"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description      = "HTTPS from Internet"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    description = "NFS from Internet"
-    from_port   = 2049
-    to_port     = 2049
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.name_prefix}-public-instance-sg"
-  }
-}
-
-resource "aws_security_group" "allow_private_instance_tls" {
-  name        = "allow_private_instance_tls"
-  description = "Allow TLS inbound traffic for Private Instances"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description = "HTTP from Private Subnet"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/24", "10.0.3.0/24"]
-  }
-
-  ingress {
-    description = "Oracle-RDS from Private Subnet"
-    from_port   = 1521
-    to_port     = 1521
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/24", "10.0.3.0/24"]
-  }
-
-  ingress {
-    description = "MYSQL/Aurora from Private Subnet"
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/24", "10.0.3.0/24"]
-  }
-
-  ingress {
-    description = "SSH from Private Subnet"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/24", "10.0.3.0/24"]
-  }
-
-  ingress {
-    description = "HTTPS from Private Subnet"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/24", "10.0.3.0/24"]
-  }
-
-  ingress {
-    description = "All ICMP - IPv4 from Private Subnet"
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["10.0.0.0/24", "10.0.3.0/24"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${var.name_prefix}-private-instance-sg"
-  }
-}
--- variables.tf
+++ variables.tf
@@ -8,17 +8,35 @@ variable "vpc_cidr" {
   description = "The VPC cidr"
 }
 
-variable "public_subnet_cidr" {
-  type        = string
-  description = "The public subnet cidr"
+variable "public_subnets" {
+  description = "A list of public subnet objects"
+  type = list(object({
+    name = string
+    cidr_block = string
+    availability_zone = string
+  }))
 }
 
-variable "private_subnet_cidr" {
-  type        = string
-  description = "The private subnet cidr"
+variable "private_subnets" {
+  description = "A list of private subnet objects"
+  type = list(object({
+    name = string
+    cidr_block = string
+    availability_zone = string
+  }))
 }
 
-variable "private_2_subnet_cidr" {
-  type        = string
-  description = "The private 2 subnet cidr"
-}
+# variable "public_subnet_cidr" {
+#   type        = string
+#   description = "The public subnet cidr"
+# }
+
+# variable "private_subnet_cidr" {
+#   type        = string
+#   description = "The private subnet cidr"
+# }
+
+# variable "private_2_subnet_cidr" {
+#   type        = string
+#   description = "The private 2 subnet cidr"
+# }
