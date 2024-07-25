da1a253c3e117b1017fb191d4ff4c8b076ecf8ee
David Omokhodion <david.omokhodion@perizer.com>
2024-07-25 13:03:16 +0100

    leave only one example directory

--- examples/bucket_policies/main.tf
+++ examples/bucket_policies/main.tf
@@ -54,6 +54,72 @@ module "s3_bucket" {
     }
   ]
 
+  lifecycle_rule = [
+
+    {
+      id = "rule-1"
+
+      transition = {
+        days          = 30
+        storage_class = "STANDARD_IA"
+      }
+
+      transition = {
+        days          = 60
+        storage_class = "GLACIER"
+      }
+
+      expiration = {
+        days = 90
+      }
+
+      filter = {
+        and = {
+          prefix                   = "logs/"
+          object_size_greater_than = 500
+          object_size_less_than    = 64000
+        }
+      }
+
+      enabled = true
+    },
+
+    {
+      id = "non-current"
+
+      noncurrent_version_expiration = {
+        noncurrent_days = 90
+      }
+
+      noncurrent_version_transition = {
+        noncurrent_days = 30
+        storage_class   = "STANDARD_IA"
+      }
+
+      noncurrent_version_transition = {
+        noncurrent_days = 60
+        storage_class   = "GLACIER"
+      }
+
+      filter = {
+        prefix = "logs/"
+      }
+
+      enabled = true
+    }
+
+  ]
+
+  # Server-side Encryption
+  server_side_encryption_configuration = {
+    rule = {
+      apply_server_side_encryption_by_default = {
+        sse_algorithm     = "aws:kms"
+        kms_master_key_id = null # The default `aws/s3` AWS KMS master key is used if this element is absent while the `sse_algorithm` is `aws:kms`.
+      }
+    }
+  }
+
   logging = {
     target_bucket = "perizer-vpclogsbucket" #module.log_bucket.s3_bucket_id
     target_prefix = "test_s3_logging/"
